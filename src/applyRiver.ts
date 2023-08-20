289eff7be0ef721b50d449e9c209a3f7785ce510
IR0NSIGHT <irnsght@googlemail.com>
2023-08-20 17:07:51 +0200

    remove dead code, old river-application

--- src/applyRiver.ts
+++ src/applyRiver.ts
@@ -11,23 +11,4 @@ export type RiverExportTarget = {
 }
 
 type AnnotationLayer = number;
-export const applyRiverToTerrain = (waterSystem: {
-    river: point[],
-    ponds: Puddle[],
-}, target: RiverExportTarget, globalPondSurface: SeenSetReadOnly): void => {
 
-    waterSystem.river
-        .filter(globalPondSurface.hasNot)   //river point is not part of a pond
-        .filter((a) => !isWater(a))
-        .map(minFilter)
-        .forEach((a) => {
-            if ( target.applyRivers)
-                setZ(a.point, a.z - 1)
-
-            if (target.applyRivers)
-                setWaterLevel(a.point, a.z )
-
-            if (target.annotationColor !== undefined)
-                dimension.setLayerValueAt(org.pepsoft.worldpainter.layers.Annotations.INSTANCE, a.point.x, a.point.y, target.annotationColor);
-        });
-};
