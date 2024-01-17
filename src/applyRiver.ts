0f69b1c2b9603bdb99ba061adfe0ad82fdf62654
IR0NSIGHT <irnsght@googlemail.com>
2024-01-17 20:15:42 +0100

    remove unused imports

--- src/applyRiver.ts
+++ src/applyRiver.ts
@@ -1,10 +1,3 @@
-import {point} from "./point";
-import {isWater, setWaterLevel, setZ} from "./terrain";
-
-import {minFilter} from "./pathing/postprocessing";
-import {Puddle} from "./puddle";
-import {SeenSetReadOnly} from "./SeenSet";
-
 export type RiverExportTarget = {
     annotationColor: AnnotationLayer | undefined,
     applyRivers: boolean
