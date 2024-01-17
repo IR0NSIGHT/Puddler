e516d535c5cd03544290c63d58b2748c373f053e
IR0NSIGHT <irnsght@googlemail.com>
2024-01-17 18:16:05 +0100

    refactor pathRiverFrom: extract function

--- src/pathing/river.ts
+++ src/pathing/river.ts
@@ -1,63 +1,40 @@
 import {makeSet, SeenSet} from "../SeenSet";
-import {
-  addPoints,
-  getNeighbourPoints,
-  getNeighbourPointsDiagonal,
-  parentedPoint,
-  parentedToList,
-  point,
-} from "../point";
+import {addPoints, getNeighbourPoints, parentedPoint, parentedToList, point,} from "../point";
 import {getZ, isWater} from "../terrain";
-import {findPondOutflow, PondGenerationParams} from "../puddle";
+import {findPondOutflow, PondGenerationParams, Puddle} from "../puddle";
 import {log} from "../log";
 
 export const testIfDownhill = (path: point[]) => {
-  for (let i = 0; i < path.length - 1; i++) {
-    const current = getZ(path[i], true);
-    const next = getZ(path[i + 1], true)
-    if (next > current)
-      return false;
-  }
-  return true;
+    for (let i = 0; i < path.length - 1; i++) {
+        const current = getZ(path[i], true);
+        const next = getZ(path[i + 1], true)
+        if (next > current)
+            return false;
+    }
+    return true;
 }
 export const annotationColor = {
-  PURPLE: 10,
-  ORANGE: 2,
-  YELLOW: 5,
-  RED: 14
+    PURPLE: 10,
+    ORANGE: 2,
+    YELLOW: 5,
+    RED: 14
 };
 
 /**
- * start a new river path at this position
- * @param pos
- * @param rivers
- * @param pondParams
+ * finds route that escapes the pond, to continue the river there
  */
-export const pathRiverFrom = (pos: point, rivers: SeenSet, pondParams: PondGenerationParams): {river: point[], ponds: {pondSurface: point[], waterLevel: number, depth: number, escapePoint: point | undefined}[] } => {
-  const path: parentedPoint[] = [{point: pos, parent: undefined, distance: -1}];
-  let safetyIt = 0;
-  let current = pos;
-  let riverMerged = false;
-  const thisRiverSet = makeSet();
-  const puddleDebugSet = makeSet();
-  const ponds = [];
-
-  while (safetyIt < 1000) {
-    safetyIt++;
-    if (getZ(current) < params.waterLevel || (params.stopOnWater && isWater(current))) //base water level reached
-        break;
-
-    let pathToDrop = findClosestDrop(current, getZ(current));
-
-    if (pathToDrop == undefined) {
-      //abort if closestDrop coulndt find anything
-      const pond = findPondOutflow([current], pondParams.maxSurface, puddleDebugSet)
-      //applyPuddleToMap(pond.pondSurface, pond.waterLevel, {annotationColor: undefined, flood: true});
-      ponds.push(pond);
-      if (pond.escapePoint !== undefined) {
+const escapePond = (pond: Puddle, startPoint: parentedPoint, puddleDebugSet: SeenSet): {
+    canEscape: boolean,
+    escapePath: parentedPoint[]
+} => {
+    if (pond.escapePoint !== undefined) { //can escape pond
         pond.pondSurface.forEach(puddleDebugSet.add);
 
-        const escapePoint: parentedPoint = {point: pond.escapePoint!, parent: path[path.length - 1], distance: -1}
+        const escapePoint: parentedPoint = {
+            point: pond.escapePoint!,
+            parent: startPoint,
+            distance: -1
+        }
 
         const thisPond = makeSet();
         pond.pondSurface.forEach(thisPond.add);
@@ -69,53 +46,91 @@ export const pathRiverFrom = (pos: point, rivers: SeenSet, pondParams: PondGener
             (p: point) => getZ(p, true) <= pond.waterLevel
         )
         if (pathEscapeToPond == undefined) {
-          log("ERROR: couldnt find path to escape point: " + JSON.stringify(escapePoint.point))
-          break;
+            log("ERROR: couldnt find path to escape point: " + JSON.stringify(escapePoint.point))
+            return {canEscape: false, escapePath: []}
         }
-        pathToDrop = pathEscapeToPond.reverse();
+        const pathToDrop = pathEscapeToPond.reverse();
         pathToDrop.shift(); //remove connectionpoint on pond surface
         pathToDrop.push(escapePoint);
-      } else {
-        break;
-      }
+        return {canEscape: true, escapePath: pathToDrop}
+    } else {
+        return {canEscape: false, escapePath: []}
     }
+}
 
+/**
+ * start a new river path at this position
+ * @param pos
+ * @param rivers
+ * @param pondParams
+ */
+export const pathRiverFrom = (pos: point, rivers: SeenSet, pondParams: PondGenerationParams): {
+    river: point[],
+    ponds: { pondSurface: point[], waterLevel: number, depth: number, escapePoint: point | undefined }[]
+} => {
+    const path: parentedPoint[] = [{point: pos, parent: undefined, distance: -1}];
+    let safetyIt = 0;
+    let current = pos;
+    let riverMerged = false;
+    const thisRiverSet = makeSet();
+    const puddleDebugSet = makeSet();
+    const ponds = [];
 
+    while (safetyIt < 1000) {
+        safetyIt++;
+        if (getZ(current) < params.waterLevel || (params.stopOnWater && isWater(current))) //base water level reached
+            break;
 
-    //add found path to riverpoint list, until water/river is reached
-    for (let point of pathToDrop) {
-      if (rivers.has(point.point)) {
-        riverMerged = true;
-        break;
-      }
+        let pathToDrop = findClosestDrop(current, getZ(current));
 
-      if (params.stopOnWater && isWater(point.point)) {
-        break;
-      }
-      path.push(point);
-      thisRiverSet.add(point.point);
-      // rivers.add(point.point);
+        if (pathToDrop == undefined) { //could not find a dropPoint => caught in hole
+            //abort if closestDrop coulndt find anything
+            const pond = findPondOutflow([current], pondParams.maxSurface, puddleDebugSet)
+            //applyPuddleToMap(pond.pondSurface, pond.waterLevel, {annotationColor: undefined, flood: true});
+            ponds.push(pond);
+
+            const escape = escapePond(pond, path[path.length - 1], puddleDebugSet)
+            if (escape.canEscape) {
+                pathToDrop = escape.escapePath
+            } else {
+                break;  //not able to continue river. abort.
+            }
+        }
+
+        //add found path to riverpoint list, until water/river is reached
+        for (let point of pathToDrop) {
+            if (rivers.has(point.point)) {
+                riverMerged = true;
+                break;
+            }
+
+            if (params.stopOnWater && isWater(point.point)) {
+                break;
+            }
+            path.push(point);
+            thisRiverSet.add(point.point);
+            // rivers.add(point.point);
+        }
+        if (riverMerged) break;
+        //end of path is droppoint
+        current = pathToDrop[pathToDrop.length - 1].point;
     }
-    if (riverMerged) break;
-    //end of path is droppoint
-    current = pathToDrop[pathToDrop.length - 1].point;
-  }
-  path.forEach((p) => rivers.add(p.point));
-  return { river: path.map((a) => a.point), ponds: ponds};
+    path.forEach((p) => rivers.add(p.point));
+    return {river: path.map((a) => a.point), ponds: ponds};
 };
 
 
 export const squaredDistanceBetweenPoints = (a: point, b: point) => {
-    const diff = {x: a.x-b.x, y: a.y-b.y};
-    return diff.x*diff.x + diff.y*diff.y;
+    const diff = {x: a.x - b.x, y: a.y - b.y};
+    return diff.x * diff.x + diff.y * diff.y;
 }
 
 
 export const insertInSortedQueue = (sortedQueue: parentedPoint[], point: parentedPoint): void => {
     let i = 0;
     for (let iteratorPoint of sortedQueue) {
-          if (iteratorPoint.distance > point.distance) break;
-          i++;
+        if (iteratorPoint.distance > point.distance) break;
+        i++;
     }
     sortedQueue.splice(i, 0, point);
 }
@@ -139,45 +154,49 @@ export function findClosestDrop(
     posZ: number,
     isDrop: (p: point) => boolean = (p) => getZ(p, true) < Math.round(posZ),
     isValidNeighbour: (p: point) => boolean = (p) => getZ(p, true) <= posZ
-): parentedPoint[]|undefined {
-  const seenSet: SeenSet = makeSet();
+): parentedPoint[] | undefined {
+    const seenSet: SeenSet = makeSet();
 
-  const queue: parentedPoint[] = [];
-  queue.push({ point: startingPoint, parent: undefined, distance: 0 })
-  seenSet.add(startingPoint);
-  let next: parentedPoint;
-  let safetyIterator = 0;
-  let searchCenter: point = startingPoint
+    const queue: parentedPoint[] = [];
+    queue.push({point: startingPoint, parent: undefined, distance: 0})
+    seenSet.add(startingPoint);
+    let next: parentedPoint;
+    let safetyIterator = 0;
+    let searchCenter: point = startingPoint
 
-  while (queue.length != 0 && safetyIterator < 50000) {
-    next = queue.shift() as parentedPoint;
-    if (isDrop(next.point)) {
-      const path = parentedToList(next, []).reverse();
-      //path starts with startingPoint, which is not wanted
-      path.shift();
-      return path;
+    while (queue.length != 0 && safetyIterator < 50000) {
+        next = queue.shift() as parentedPoint;
+        if (isDrop(next.point)) {
+            const path = parentedToList(next, []).reverse();
+            //path starts with startingPoint, which is not wanted
+            path.shift();
+            return path;
+        }
+
+        let neighbours = getNeighbourPoints(next.point).filter(seenSet.hasNot);
+        const trueLowerNeighbours = neighbours.filter(n => getZ(n, true) < Math.round(posZ));
+        if (trueLowerNeighbours.length == 0) {
+            //no lower neighbour, river is in flat area => sort by tinyest height difference
+            neighbours = neighbours.sort((a, b) => {
+                const aZ = getZ(a, false);
+                const bZ = getZ(b, false);
+                return aZ - bZ;
+            });
+        }
+
+        neighbours.filter(isValidNeighbour).forEach((n) => {
+            seenSet.add(n);
+            insertInSortedQueue(queue, {
+                point: n,
+                parent: next,
+                distance: squaredDistanceBetweenPoints(n, searchCenter)
+            });
+        });
+        safetyIterator++;
     }
-
-    let neighbours = getNeighbourPoints(next.point).filter(seenSet.hasNot);
-    const trueLowerNeighbours = neighbours.filter(n => getZ(n, true) < Math.round(posZ));
-    if (trueLowerNeighbours.length == 0) {
-      //no lower neighbour, river is in flat area => sort by tinyest height difference
-      neighbours = neighbours.sort((a, b) => {
-        const aZ = getZ(a, false);
-        const bZ = getZ(b, false);
-        return aZ - bZ;
-      });
-    }
-
-    neighbours.filter(isValidNeighbour).forEach((n) => {
-      seenSet.add(n);
-      insertInSortedQueue(queue, {point: n, parent: next, distance: squaredDistanceBetweenPoints(n, searchCenter)});
-    });
-    safetyIterator++;
-  }
-  return undefined;
+    return undefined;
 }
 
 export const capRiverStart = (river: point[], slice: number) => {
-  return river.slice(slice);
+    return river.slice(slice);
 };
